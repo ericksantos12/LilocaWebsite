dc2cc716e9ddd709a05ffb63ad5ab2c697b52047
Erick Santos <erick.sousa@hotmail.com.br>
2022-11-26 01:31:33 -0300

    ♻️ implementando procs + limpando codigo

--- php/inclui_pedido.php
+++ php/inclui_pedido.php
@@ -2,57 +2,46 @@
 require_once 'conectaBD.php';
 require_once 'utils.php';
 
-// TODO: Fazer stored procedure inserção de pedido e associativa com produto
-// TODO: Integrar stored procedure com o código
-
 try {
 
     $produtos = ['baloes', 'docinhos', 'marshmallow', 'salgadinhos', 'coca350ml', 'coca2l', 'minisanduiche', 'toalhacolorida', 'boloprestigio', 'bolotrufado', 'tortaholandesa', 'copodescartavel', 'pratodescartavel', 'talherdescartavel'];
     
     $festa = $_POST['festa'];
-    $dataPedido = date('Y-m-d');
     $tipoEntrega = $_POST['tipoentrega'];
     $endereco = $_POST['endereco'];
     $prazoEntrega = $_POST['prazo'];
     $frete = $tipoEntrega == 'entrega' ? 20 : 0;
-    $valorProdutos = 0;
     $sinal = 0;
-    $total = 0;
-
-    // $query = "INSERT INTO `pedido`(`Festa_idFesta`, `dataPed`, `tipoDeEntrega`, `endDeEntrega`, `prazoDeEntrega`, `frete`, `sinal`, `restante`, `dataDeEntrega`, `total`) VALUES ('".$festa."', '". $dataPedido ."', '".$tipoEntrega."', '".$endereco."', '".$prazoEntrega."', '".$frete."', '".$sinal."', '".$sinal."', null, '".$total."');";
 
     $conn->beginTransaction();
 
-    // prepara a query inserção de pedido
-    $query = $conn->prepare("INSERT INTO `pedido`(`Festa_idFesta`, `dataPed`, `tipoDeEntrega`, `endDeEntrega`, `prazoDeEntrega`, `frete`, `sinal`, `restante`, `dataDeEntrega`, `total`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, null, ?);");
-
-    // executa a query
-    $query->execute([$festa, $dataPedido, $tipoEntrega, $endereco, $prazoEntrega, $frete, $sinal, $sinal, $total]);
-    $lastID = $conn->lastInsertId(); // pega o id do pedido
-
-    //prepara a query inserção na tabela associativa
-    $query = $conn->prepare("INSERT INTO `pedido_has_produtos`(`Pedido_idPedido`, `Produtos_idProdutos`, `quantidade`, `valorQtd`) VALUES (?, ?, ?, ?);");
-
     for ($i=0; $i < count($produtos); $i++) { 
-        $produtoQtd = (int)$_POST[$produtos[$i]]; // pega a quantidade de produtos escolhidos no forms
+        $produtoQtd = (int)$_POST[$produtos[$i]];
 
-        if ($produtoQtd != 0){ // verifica se a quantidade nao é 0
-
-            // Pega o valor unitário do produto e múltiplica pela quantidade selecionada
-            $valorQtd = fetchSelect($conn, "SELECT `valorUnit` FROM `produtos` WHERE idProdutos = ?", [$i + 1])[0][0] * $produtoQtd;
-
-            $query->execute([$lastID, $i + 1, $produtoQtd, $valorQtd]); // executa a inserção na tabela associativa
-
-            $valorProdutos += $valorQtd; // soma o valor final na variavel total
+        if ($produtoQtd != 0) {
+            // pega o valor unitario de cada produto e multiplica por sua quantidade, somando-os na variável sinal
+            $sinal += fetchSelect($conn, "SELECT valorUnit FROM produtos WHERE idProdutos = ?;", [$i + 1])[0][0] * $produtoQtd;
         }
     }
+    $sinal /= 2;
 
-    $sinal = $valorProdutos / 2; // divide o sinal 50%
-    $total = $frete + $valorProdutos; // calcula o total com base no valor total de produtos + o frete
+    // prepara inserção no pedido: idFesta, tipo, endereco, prazo, frete, sinal
+    $query = $conn->prepare("CALL Inclui_Pedido(?, ?, ?, ?, ?, ?, @LastID);"); // <-- poe pra fora variavel lastID com o ultimo id de pedido
+    $query->execute([$festa, $tipoEntrega, $endereco, $prazoEntrega, $frete, $sinal]);
 
-    // prepara query para dar update no ultimo pedido inserido
-    $query = $conn->prepare("UPDATE `pedido` SET `sinal`= ? ,`restante`= ? , `total` = ? WHERE `idPedido`= ? ");
-    $query->execute([$sinal, $sinal, $total, $lastID]); // adiciona o valor do sinal e total
+    $query2 = $conn->query("SELECT @LastID AS id;"); // <-- pega o valor da variavel LastID
+    $lastID = $query2->fetch(PDO::FETCH_NUM)[0];
+
+    // prepara inserção na associativa: idPed, idProd, qtd
+    $query = $conn->prepare("CALL Inclui_PedidoProduto(?, ?, ?);");
+
+    for ($i=0; $i < count($produtos); $i++) { 
+        $produtoQtd = (int)$_POST[$produtos[$i]];
+
+        if ($produtoQtd != 0){
+            $query->execute([$lastID, $i + 1, $produtoQtd]); // executa a inserção na tabela associativa
+        }
+    }
     
     $conn->commit(); //fim
 
