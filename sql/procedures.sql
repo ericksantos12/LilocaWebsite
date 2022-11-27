7207be251a4a6a0e6e5cbf4989a1a80a8655ebbe
Erick Santos <erick.sousa@hotmail.com.br>
2022-11-26 21:02:02 -0300

    ✨ arquivo com procedures usados

---
+++ sql/procedures.sql
@@ -0,0 +1,49 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Consultar_Pedido`(IN `id` INT)
+BEGIN
+
+SELECT `aniversariante`, `dataPed`, `tipoDeEntrega`, `endDeEntrega`, `prazoDeEntrega`, `frete`, `sinal`, `restante`, `dataDeEntrega`, `total` 
+FROM `pedido` 
+INNER JOIN festa 
+WHERE `Festa_idFesta`=`idFesta` AND `idPedido`=id;
+
+END$$
+DELIMITER ;
+-- // ----------------------------------------------------------------------------------------------------------------------// --
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Consultar_PedidoProduto`(IN `id` INT)
+BEGIN
+
+SELECT item, `quantidade`, `valorQtd` 
+FROM pedido_has_produtos 
+INNER JOIN produtos 
+WHERE pedido_has_produtos.Produtos_idProdutos=produtos.idProdutos AND pedido_has_produtos.Pedido_idPedido = id;
+
+END$$
+DELIMITER ;
+-- // ----------------------------------------------------------------------------------------------------------------------// --
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Inclui_Pedido`(IN `idFesta` INT, IN `tipo` VARCHAR(225), IN `endereco` VARCHAR(255), IN `prazo` DATETIME, IN `frete` DOUBLE, IN `sinal` DOUBLE, OUT `LastID` INT)
+BEGIN
+
+INSERT INTO `pedido`(`Festa_idFesta`, `dataPed`, `tipoDeEntrega`, `endDeEntrega`, `prazoDeEntrega`, `frete`, `sinal`, `restante`, `dataDeEntrega`, `total`) VALUES (idFesta, CURRENT_DATE(), tipo, endereco, prazo, frete, sinal, sinal, null, frete + (sinal * 2));
+
+SET LastID = LAST_INSERT_ID();
+
+END$$
+DELIMITER ;
+-- // ----------------------------------------------------------------------------------------------------------------------// --
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Inclui_PedidoProduto`(IN `idPed` INT, IN `idProd` INT, IN `qtd` INT)
+BEGIN
+
+SELECT valorUnit INTO @valorProduto FROM produtos WHERE produtos.idProdutos = idProd;
+
+INSERT INTO `pedido_has_produtos`(`Pedido_idPedido`, `Produtos_idProdutos`, `quantidade`, `valorQtd`) VALUES (idPed, idProd, qtd, @valorProduto * qtd);
+
+END$$
+DELIMITER ;
+-- // ----------------------------------------------------------------------------------------------------------------------// --
